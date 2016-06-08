78577cf0a1a8831d39c583e897c089aba198b5f5
schein00 <schein.jefer@gmail.com>
2016-06-08 20:33:23 -0300

    atualizacao mem.c

--- mem.c
+++ mem.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
 
+
+#define TCACHE 16
+#define TMEM 256
 
 /* Grupos 5: IVAIR PUERARI, JEFERSON AUGUSTO SCHEIN e GABRIEL FERNANDO CASAGRANDE
 Politica de Mapeamento: Totalmente associativo
@@ -9,23 +13,42 @@ Politica de Troca: Aleatório
 Politica de escrita:  Write - Through */ 
 
 
-typedef struct MemCahe{
+typedef struct MemCache{
 	int valBit;  		// se 0 sem informacao, 1 informacao presente
 	int deslocamento;	// deslocamento dentro do bloco da memoria
 	int endereco; 		// endereco do comeco do bloco da memorio
-	char info[50]
-}MemCahe;
+	int info;
+}MemCache;
  
 typedef struct MemPrincipal{
-	char info[50];
+	int info;
 }MemPrincipal;
+
+void iniciaCache(MemCache cache[TCACHE]){
+	int i;
+	for(i = 0; i < TCACHE; i ++){
+		cache[i].valBit = 0;
+	}
+}
+
+void populaMem(MemPrincipal memoria[TMEM]){
+	srand(time(NULL));
+	int i;
+
+	for(i = 0; i < TMEM; i++){
+		memoria[i].info = rand() % 100000;
+	}
+}
+
+
 
 
 int main(){
+	MemCache cache[TCACHE];
+	MemPrincipal memoria[TMEM];
 
-	MemCache cache[16];
-	MemPrincipal principal[256];
-
+	iniciaCache(cache);
+	populaMem(memoria);
 
 	return 0;
-};
+}
