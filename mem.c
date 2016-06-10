6680ff80b0a1803c34f142995113be20ed4cb35c
schein00 <schein.jefer@gmail.com>
2016-06-10 19:32:47 -0300

    Update mem.c

--- mem.c
+++ mem.c
@@ -6,19 +6,20 @@
 
 #define TCACHE 16
 #define TMEM 256
+#define TBLOCO 5
+#define TDESLOCAMENTO 3
 
 /* Grupos 5: IVAIR PUERARI, JEFERSON AUGUSTO SCHEIN e GABRIEL FERNANDO CASAGRANDE
 Politica de Mapeamento: Totalmente associativo
 Politica de Troca: Aleatório
 Politica de escrita:  Write - Through */ 
 
-
 typedef struct MemCache{
 	int valBit;  		// se 0 sem informacao, 1 informacao presente
 	int deslocamento;	// deslocamento dentro do bloco da memoria
 	int deslocamentoBin[3];	// deslocamento em binario
-	int endereco; 		// endereco do comeco do bloco da memorio
-	int enderecoBin[5];	// endereco em binario 	
+	int bloco; 		// endereco do comeco do bloco da memorio
+	int blocoBin[5];	// endereco em binario 	
 	int info;
 	
 }MemCache;
@@ -26,6 +27,11 @@ typedef struct MemCache{
 typedef struct MemPrincipal{
 	int info;
 }MemPrincipal;
+
+int acessos = 0, acertos = 0, faltas = 0;
+int leituras = 0, escritas = 0;
+int aLeituras = 0, fLeituras = 0;
+int aEscrita = 0,fEscrita = 0;
 
 void iniciaCache(MemCache cache[TCACHE]){
 	int i;
@@ -43,8 +49,100 @@ void populaMem(MemPrincipal memoria[TMEM]){
 	}
 }
 
+void binBloco( int d[TBLOCO], int x ){
+	int i;
+	int r[TBLOCO];
+
+	for(i = 0; i < TBLOCO; i ++){
+		r[i] = 0;
+	}	
+
+	for(i = 0; x != 0 && i < TBLOCO; i++){
+		if(x == 0){				
+			r[i] = 1;
+		}else if((x % 2) == 0){
+			r[i] = 0;		
+		}else if((x % 2) == 1){
+			r[i] = 1;		
+		}
+		x = x / 2;
+	}
+	x = 4;
+	for(i = 0; i < TBLOCO; i ++){
+		d[i] = r[x--];
+	}
+}
+
+void binDeslocamento( int d[TBLOCO], int x ){
+	int i;
+	int r[TDESLOCAMENTO];
+
+	for(i = 0; i < TDESLOCAMENTO; i ++){
+		r[i] = 0;
+	}	
+
+	for(i = 0; x != 0 && i < TDESLOCAMENTO; i++){
+		if(x == 0){				
+			r[i] = 1;
+		}else if((x % 2) == 0){
+			r[i] = 0;		
+		}else if((x % 2) == 1){
+			r[i] = 1;		
+		}
+		x = x / 2;
+	}
+	x = 2;
+	for(i = 0; i < TDESLOCAMENTO; i ++){
+		d[i] = r[x--];
+	}	
+
+}
+
 void LerMem(int endereco, MemPrincipal memoria[TMEM], MemCache cache[TCACHE]){
-	int d = 0, b = 0;
+	leitura++;
+	int d = 0, b = 0, i, p = 0;
+
+	d = endereco % 8;
+	b = endereco / 8;
+
+/*
+	printf("Bloco: %d\n", b);
+	binBloco(cache[0].enderecoBin, b);
+
+	for(i = 0; i < TBLOCO; i ++){
+		printf("%d", cache[0].enderecoBin[i]);
+	}
+	printf("\n");
+	printf("Deslocamento: %d\n", d);
+	binDeslocamento(cache[0].deslocamentoBin, d);
+	
+	for(i = 0; i < TDESLOCAMENTO; i ++){
+		printf("%d", cache[0].deslocamentoBin[i]);
+	}
+*/
+
+	for(i = 0; i < TCACHE; i ++){
+		if(cache[i].valBit !=0){
+			if(cache[i].bloco == b && cache[i].deslocamento == d){
+				acerto++;
+				aLeitura++;
+				printf("\n");	
+				printf("%d\n\n", cache[i].info);
+				return;
+			}			
+		}
+	}
+	falta++;
+	fLeitura++;
+	p = rand() % 16;
+	cache[p].valBit = 1;
+	cache[p].info = memoria[endereco].info;
+	cache[p].bloco = b;
+	cache[p].deslocamento = d;
+	binDeslocamento(cache[p].deslocamentoBin, d);
+	binBloco(cache[0].enderecoBin, b);
+
+	scanf("%d", &d);
 
 	return;
 }
@@ -54,6 +152,8 @@ void EscreverMem(int info, int endereco, MemPrincipal memoria[TMEM], MemCache ca
 }
 
 int main(){
+	srand(time(NULL));
+	
 	MemCache cache[TCACHE];
 	MemPrincipal memoria[TMEM];
 
@@ -61,7 +161,7 @@ int main(){
 	populaMem(memoria);
 
 	int menu = 100, sub = 100, endereco = 0, info;
-
+	
 	while(menu){
 		system("clear");
 		printf("=========================================================================\n");
@@ -80,12 +180,14 @@ int main(){
 				printf("OBS: O endereco da memoria que sera lido\n Endereco deve ser em decimal!\n Endereco: ");
 				scanf("%d", &endereco);
 				LerMem(endereco, memoria, cache);
+				acesso++;
 				break;
 			case 2:
 				printf("Digite a informação a ser escrita na Memoria: ");
 				scanf("%d", &info);
 				printf("OBS: O endereco da memoria onde sera escrita a informação\n Endereco deve ser em decimal!\n Endereco: ");
 				scanf("%d", &endereco);
+				acesso++;
 				EscreverMem(info, endereco, memoria, cache);
 				break;
 			case 3: 
